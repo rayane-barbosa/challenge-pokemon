91e05f1ab17ccae7b1ae5fbfcb12a2562e73769d
Rayane-Barbosa <rayane.deavelardejesus@ab-inbev.com>
2023-11-01 15:42:17 -0300

    [Implementing-API] feat: extract image URL from API and set as <img> src attribute

--- src/api.js
+++ src/api.js
@@ -1,7 +1,16 @@
 async function fetchPokemonImage(pokemonName) {
-    try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-        const data = await response.json();
-        }
-    
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+    );
+    const data = await response.json();
+
+    const imageUrl = data.sprites.front_default;
+    const pokeImageElement = document.getElementById("poke-image");
+    pokeImageElement.src = imageUrl;
+  } catch (error) {
+    console.error("Error fetching Pokemon image: ", error);
+  }
 }
+
+export { fetchPokemonImage };
